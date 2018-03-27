75b96e91ef828472b96599416e73a3c884fc5e6e
Dmitriy Romanov <30533564+romanovd1313@users.noreply.github.com>
2018-03-27 17:25:20 +0300

    75675

--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
 'use strict';
 
 importScripts('/sw-toolbox.js');
-
-toolbox.precache(["index.html","css/style.css","about.html","blog.html","contact.html","portfolio.html"]);
-
+toolbox.precache(["index.html"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
-
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
 });
